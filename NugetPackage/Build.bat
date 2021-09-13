46c3a751d6f26e9ae74e70f07ba1beb7a59196ea
Andrew <zdyrski.andrew@gmail.com>
2021-09-13 16:44:53 -0700

    Nuget package generation files

---
+++ NugetPackage/Build.bat
@@ -0,0 +1,6 @@
+rem Runs the build process that creates the NuGet and Umbraco packages
+rem to debug add /bl to the MSBuild.exe task and use http://msbuildlog.com/ to view the binary log
+
+rem Update this path to MSBuild if different on your system. Use a recent version that recognises latest C# syntax.
+
+Call "%programfiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" package.build.xml /bl /p:Configuration=Release
